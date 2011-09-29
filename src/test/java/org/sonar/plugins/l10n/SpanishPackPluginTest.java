820b7a79d2ed10192cca84ac8714ccc17ff8633b
Simon Brandhof <simon.brandhof@gmail.com>
2011-09-29 16:03:25 +0000

    Add unit test to the Spanish pack

---
+++ src/test/java/org/sonar/plugins/l10n/SpanishPackPluginTest.java
@@ -0,0 +1,48 @@
+/*
+ * L10n :: Spanish Pack
+ * Copyright (C) 2011 eXcentia
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.plugins.l10n;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.sonar.test.i18n.I18nMatchers.isBundleUpToDate;
+
+public class SpanishPackPluginTest {
+
+  private static final String SONAR_VERSION = "2.10";
+
+  @Ignore("Disabled because of https://jira.codehaus.org/browse/SONAR-2846. Will be reactivated with sonar-testing-harness 2.12.")
+  @Test
+  public void coreBundleShouldBeUpToDate() {
+    assertThat("core_es.properties", isBundleUpToDate(SONAR_VERSION));
+  }
+
+  @Test
+  public void gwtBundleShouldBeUpToDate() {
+    assertThat("gwt_es.properties", isBundleUpToDate(SONAR_VERSION));
+  }
+
+  @Test
+  public void squidJavaBundleShouldBeUpToDate() {
+    assertThat("squidjava_es.properties", isBundleUpToDate(SONAR_VERSION));
+  }
+
+}
