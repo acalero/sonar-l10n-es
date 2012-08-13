67bf8e1d905e57e27ff9ddcef94a6784f45c21bd
Fabrice Bellingard <bellingard@gmail.com>
2012-08-13 14:37:12 +0000

    SONARPLUGINS-2142 Move L10n bundles into their corresponding Language Packs

--- src/test/java/org/sonar/plugins/l10n/SpanishPackPluginTest.java
+++ src/test/java/org/sonar/plugins/l10n/SpanishPackPluginTest.java
@@ -41,6 +41,7 @@ public class SpanishPackPluginTest {
     {
       put("abacus", "http://svn.codehaus.org/sonar-plugins/tags/sonar-abacus-plugin-0.1/src/main/resources/org/sonar/l10n/abacus.properties");
       put("branding", "http://svn.codehaus.org/sonar-plugins/tags/sonar-branding-plugin-0.3/src/main/resources/org/sonar/l10n/branding.properties");
+      put("jira", "http://svn.codehaus.org/sonar-plugins/tags/sonar-jira-plugin-1.0/src/main/resources/org/sonar/l10n/jira.properties");
     }
   };
 
