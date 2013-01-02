8276777c788a6413a20ab04e72dc2efc7b23d636
Antonio Calero <acalero@excentia.es>
2013-01-02 17:21:08 +0100

    Changed to use new test environment.

--- src/test/java/org/sonar/plugins/l10n/SpanishPackPluginTest.java
+++ src/test/java/org/sonar/plugins/l10n/SpanishPackPluginTest.java
@@ -20,40 +20,13 @@
 package org.sonar.plugins.l10n;
 
 import org.junit.Test;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.sonar.test.i18n.I18nMatchers.assertAllBundlesUpToDate;
+import org.sonar.test.i18n.I18nMatchers;
 
 public class SpanishPackPluginTest {
 
-  /**
-   * Version of Sonar which is covered by the language pack
-   */
-  private static final String SONAR_VERSION = "3.2";
-
-  /**
-   * Bundles of the forge plugins covered by the language pack
-   */
-
-  private static final Map<String, String> pluginIdsToBundleUrlMap = new HashMap<String, String>() {
-    {
-      put("abacus", "http://svn.codehaus.org/sonar-plugins/tags/sonar-abacus-plugin-0.1/src/main/resources/org/sonar/l10n/abacus.properties");
-      put("branding", "http://svn.codehaus.org/sonar-plugins/tags/sonar-branding-plugin-0.3/src/main/resources/org/sonar/l10n/branding.properties");
-      put("jira", "http://svn.codehaus.org/sonar-plugins/tags/sonar-jira-plugin-1.0/src/main/resources/org/sonar/l10n/jira.properties");
-      put("uselesscodetracker", "http://svn.codehaus.org/sonar-plugins/tags/sonar-useless-code-tracker-plugin-0.5/src/main/resources/org/sonar/l10n/uselesscodetracker.properties");
-      put("violationdensity", "http://svn.codehaus.org/sonar-plugins/tags/sonar-violationdensity-plugin-1.2/src/main/resources/org/sonar/l10n/violationdensity.properties");
-      put("widgetlab", "http://svn.codehaus.org/sonar-plugins/tags/sonar-widget-lab-plugin-1.3/src/main/resources/org/sonar/l10n/widgetlab.properties");
-      
-      // TODO : following plugins have currently no released version to check against => update when a first release with bundles is done.
-      put("tabmetrics", "http://svn.codehaus.org/sonar-plugins/trunk/tab-metrics/src/main/resources/org/sonar/l10n/tabmetrics.properties");
-    }
-  };
-
   @Test
-  public void test() throws Exception {
-    assertAllBundlesUpToDate(SONAR_VERSION, pluginIdsToBundleUrlMap);
+  public void bundles_should_be_up_to_date() {
+    I18nMatchers.assertBundlesUpToDate();
   }
 
 }
